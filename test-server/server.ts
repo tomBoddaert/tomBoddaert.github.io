a8a0dc7e9e6d1e04cdf5e68106d17823f92f8713
Tom Boddaert <70723965+tomBoddaert@users.noreply.github.com>
2022-07-03 21:21:42 +0100

    Adding SonarLint

--- test-server/server.ts
+++ test-server/server.ts
@@ -6,7 +6,7 @@ const port = 8080;
 const app = express();
 
 app.get( '/', ( _, res ) => {
-    readFile( 'docs/index.html', { encoding: 'utf-8' }, ( _, data ) => {
+    readFile( 'docs/index.html', { encoding: 'utf-8' }, ( _1, data ) => {
         res.send( data );
     } );
 } );
@@ -14,7 +14,7 @@ app.get( '/', ( _, res ) => {
 app.use( '/', express.static( './docs', { index: false, extensions: [ 'html' ] } ) );
 
 app.get( '*', ( _, res ) => {
-    readFile( 'docs/404.html', { encoding: 'utf-8' }, ( _, data ) => {
+    readFile( 'docs/404.html', { encoding: 'utf-8' }, ( _1, data ) => {
         res.send( data );
     } );
 } );
