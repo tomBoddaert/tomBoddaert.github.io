e5777f8d77d3f8c92ced78228192994bfbe513ef
Tom Boddaert <70723965+tomBoddaert@users.noreply.github.com>
2024-09-04 22:07:31 +0100

    Lazy load the BrainMade logo and make a post clearer

--- docs/resources/main.js
+++ docs/resources/main.js
@@ -1,8 +1,13 @@
 const [htmlElement] = document.getElementsByTagName('html');
 const fontElement = document.getElementById('FontStyle');
+const brainMadeLink = document.querySelector('#BrainMade a');
+const [brainMadeNoScript] = brainMadeLink.getElementsByTagName('noscript');
 // Switch the preload link to a stylesheet to use it
 fontElement.setAttribute('rel', 'stylesheet');
 fontElement.removeAttribute('as');
+if (brainMadeNoScript)
+    // 'noscript' elements aren't always evaluated, so use HTML
+    brainMadeLink.innerHTML = brainMadeNoScript.innerHTML;
 const url = new URL(window.location.href);
 const prideParam = url.searchParams.get('pride');
 if (prideParam !== null)
--- raw/resources/main.ts
+++ raw/resources/main.ts
@@ -1,9 +1,16 @@
 const [ htmlElement ] = document.getElementsByTagName( 'html' );
 const fontElement = document.getElementById( 'FontStyle' ) as HTMLLinkElement;
+const brainMadeLink = document.querySelector( '#BrainMade a' ) as HTMLAnchorElement;
+const [ brainMadeNoScript ] = brainMadeLink.getElementsByTagName( 'noscript' );
 
 // Switch the preload link to a stylesheet to use it
-fontElement.setAttribute('rel', 'stylesheet');
-fontElement.removeAttribute('as');
+fontElement.setAttribute( 'rel', 'stylesheet' );
+fontElement.removeAttribute( 'as' );
+
+if ( brainMadeNoScript )
+  // 'noscript' elements aren't always evaluated, so use HTML
+  brainMadeLink.innerHTML = brainMadeNoScript.innerHTML;
+
 
 const url = new URL( window.location.href );
 const prideParam = url.searchParams.get( 'pride' );
